da082cf202838734bcfecaab62161f5ca8d53101
maxim-bogdanov <937933mb@mail.ru>
2020-09-16 14:45:03 +0300

    upgradeData

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,6 +4,8 @@ import $ from 'jquery';
 
 global.jQuery = global.$ = $;
 
+
+
 $.when(isDocumentReady())
   .done(onDocumentReady);
 
@@ -11,7 +13,37 @@ function onDocumentReady() {
   if ($.fn.initPlugins) {
     $(document.body).initPlugins();
   }
+
   $(document.documentElement).trigger("document:ready");
+
+  $.getJSON('data/data.json', function (data) {
+
+    // console.log(JSON.parse((JSON.stringify(data))));
+    let newData = upgradeData(data);
+    console.log(newData);
+  });
+
+  function upgradeData(data) {
+    data = JSON.parse((JSON.stringify(data)));
+    let newData = {};
+
+    getProp(data);
+ 
+    return newData;
+
+    	
+    function getProp(o) {
+      for (let prop in o) {
+          if (typeof(o[prop]) === 'object') {
+            getProp(o[prop]);
+          } else {
+            let id = o["id"];
+            if (!newData[id]) newData[id] = {};
+            newData[id][prop] = o[prop];
+          }
+      }
+    }
+  }
 }
 
 
